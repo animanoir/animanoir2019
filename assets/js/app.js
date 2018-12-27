cac7f55b3e5f73ff16ff4a15d60f1d6a8f6a0a2b
Oscar <etherealflux94@gmail.com>
2018-12-27 02:39:05 -0600

    ya está maso el layout de un post

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,17 @@
 window.onload = function () {
 
-let arrayPalabras = ['Sé muchos secretos', 'Somos seres espirituales con experiencia humana', 'Sexo, muerte y matemáticas', 'Éste es el link al inicio', 'Per aspera ad astra'];    
+    // Pequeño script que cambia el texto cuando metes el mouse.
+    let arrayPalabras = ['Sé muchos secretos. ¿Sabes más secretos que yo?', 'Somos seres espirituales con experiencia humana', 'Sexo, muerte y matemáticas', 'Éste es el link al inicio', 'Per aspera ad astra', 'Paciencia y persistencia', '¿Te quieres casar conmigo?'];
+    
 
-let menuLatInicio = document.querySelector('#menu-lat-inicio');
+    let menuLatInicio = document.querySelector('#menu-lat-inicio');
 
-menuLatInicio.onmouseenter = function(){
-    let i = Math.floor((Math.random()*arrayPalabras.length));
-    menuLatInicio.textContent = arrayPalabras[i];
-}
+    menuLatInicio.onmouseenter = function () {
+        let i = Math.floor((Math.random() * arrayPalabras.length));
+        menuLatInicio.textContent = arrayPalabras[i];
+    }
 
-menuLatInicio.onmouseleave = function(){
-    menuLatInicio.textContent = 'Animanoir';
-}
+    menuLatInicio.onmouseleave = function () {
+        menuLatInicio.textContent = 'Animanoir';
+    }
 }
