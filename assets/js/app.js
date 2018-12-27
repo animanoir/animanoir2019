2dff230afbef0df049d40653b2535188b4213694
Oscar <etherealflux94@gmail.com>
2018-12-27 00:48:31 -0600

    quedó el menú lateral vergón

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
 window.onload = function () {
 
+let arrayPalabras = ['Sé muchos secretos', 'Somos seres espirituales con experiencia humana', 'Sexo, muerte y matemáticas', 'Éste es el link al inicio', 'Per aspera ad astra'];    
 
+let menuLatInicio = document.querySelector('#menu-lat-inicio');
 
+menuLatInicio.onmouseenter = function(){
+    let i = Math.floor((Math.random()*arrayPalabras.length));
+    menuLatInicio.textContent = arrayPalabras[i];
+}
+
+menuLatInicio.onmouseleave = function(){
+    menuLatInicio.textContent = 'Animanoir';
+}
 }
