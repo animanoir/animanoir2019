408382002481ab5a795222fcfa763e7eefc9f2ee
Oscar <etherealflux94@gmail.com>
2018-12-27 03:35:17 -0600

    mejoras

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,19 @@
 window.onload = function () {
 
     // Pequeño script que cambia el texto cuando metes el mouse.
-    let arrayPalabras = ['Sé muchos secretos. ¿Sabes más secretos que yo?', 'Somos seres espirituales con experiencia humana', 'Sexo, muerte y matemáticas', 'Éste es el link al inicio', 'Per aspera ad astra', 'Paciencia y persistencia', '¿Te quieres casar conmigo?'];
-    
+    let arrayPalabras = [
+        'Sé muchos secretos. ¿Sabes más secretos que yo?',
+        'Somos seres espirituales con experiencia humana.',
+        'Sexo, muerte y matemáticas.',
+        'Per aspera ad astra.',
+        'Paciencia y persistencia.',
+        '¿Te quieres casar conmigo?',
+        'Dómr um dauðan hvern.',
+        'Aphex Twin - 4',
+        'La armonía es caos domesticado.',
+        'Safety In Solitude',
+        'De una sola fuente todas las cosas dependen.'
+    ];
 
     let menuLatInicio = document.querySelector('#menu-lat-inicio');
 
